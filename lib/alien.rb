0d8382b95cecdac5023b497b7ea8746d8d9b3386
Artur Rodrigues <arturhoo@gmail.com>
2014-03-15 16:59:42 -0300

    Encapsulate alien's code into a module

--- lib/alien.rb
+++ lib/alien.rb
@@ -1,4 +1,10 @@
 require "alien/version"
+require "alien/alienblocktimer"
+require "alien/alienconfig"
+require "alien/alienconnection"
+require "alien/alienreader"
+require "alien/alientag"
+require "alien/alientaglist"
 
 module Alien
   # Your code goes here...
--- lib/alien/alientaglist.rb
+++ lib/alien/alientaglist.rb
@@ -1,6 +1,6 @@
 =begin rdoc
-=Alien Ruby RFID Library 
-==alientaglist.rb	
+=Alien Ruby RFID Library
+==alientaglist.rb
 
 A simple class to hold an array of taglist data elements.
 
@@ -11,53 +11,55 @@ Copyright 2008, Alien Technology Corporation. All rights reserved.
 
 #Takes a string returned from a Taglist function call and builds an array of tags.
 
-class AlienTagList < Array
-	
-	def initialize(taglist_string="")
-		super()
-		string_to_taglist(taglist_string) if taglist_string != ""
-	end
-	
+module Alien
+  class AlienTagList < Array
 
-	# Takes a taglist string from a reader and appends it to the array.
-	def string_to_taglist(taglist_string)
-		lines = taglist_string.split("\r\n")
-		
-		lines.each do |line|
-			if line != "(No Tags)"
-				add_tag(AlienTag.new(line))
-			end
-		end
-		
-		return self
-	end
-	
-	# Adds an AlienTag to the list.
-	def add_tag(t)
-		self.push(t)
-		return self  
-	end
-	
-	# A little regular expression scanner. Looks at the list of tags and returns a
-	# new taglist containing those tag IDs that match a regular expression filter.
-	def filter(filter)
-		tl = AlienTagList.new
-		
-		self.each do |ele|
-			if ele.tag =~ filter 
-				tl.add_tag(ele)
-			end
-		end
-		
-		return tl
-	end
-	
-	# A self-modifying version of filter_taglist.
-	# Excercise caution. Elements in the taglist array that do not match
-	# the regular expression are deleted.
-	def filter!(filter)
-		self.delete_if { |ele| !(ele.tag =~ filter)}
-		return self
-	end
+    def initialize(taglist_string="")
+      super()
+      string_to_taglist(taglist_string) if taglist_string != ""
+    end
 
-end #class AlienTagList
+
+    # Takes a taglist string from a reader and appends it to the array.
+    def string_to_taglist(taglist_string)
+      lines = taglist_string.split("\r\n")
+
+      lines.each do |line|
+        if line != "(No Tags)"
+          add_tag(AlienTag.new(line))
+        end
+      end
+
+      return self
+    end
+
+    # Adds an AlienTag to the list.
+    def add_tag(t)
+      self.push(t)
+      return self
+    end
+
+    # A little regular expression scanner. Looks at the list of tags and returns a
+    # new taglist containing those tag IDs that match a regular expression filter.
+    def filter(filter)
+      tl = AlienTagList.new
+
+      self.each do |ele|
+        if ele.tag =~ filter
+          tl.add_tag(ele)
+        end
+      end
+
+      return tl
+    end
+
+    # A self-modifying version of filter_taglist.
+    # Excercise caution. Elements in the taglist array that do not match
+    # the regular expression are deleted.
+    def filter!(filter)
+      self.delete_if { |ele| !(ele.tag =~ filter)}
+      return self
+    end
+
+  end #class AlienTagList
+end
